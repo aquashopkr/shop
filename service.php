2069ec82ddaa0c948cbcbdb4211ceff6cd7943ea
aquashopkr <144537793+aquashopkr@users.noreply.github.com>
2023-11-06 17:56:44 +0900

    Add files via upload

---
+++ service.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<!--만든이 : 치즈퍼리-->
+<head>
+<lang="ko">
+<meta charset="utf-8">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+  <title>AQUA STUDIOㅣ최강 봇 메이킹 스튜디오</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class="navbars">
+  <img src="https://cdn.discordapp.com/attachments/1163457070591393815/1170237879549710366/SGN_08_16_2024_1723810938900.png?ex=65584ff4&is=6545daf4&hm=c488a43be0f428ce29e9ab61a59fb633ce98284e081b6d5c93c4b813a2f2506c&" alt="내비게이션바 로고" class="nvbimage">
+</div>
+<div class="navbar">
+  <a href="#home">홈</a>
+  <a href="#news">커뮤니티</a>
+  <a href="#contact">서비스</a>
+  <a href="#contact">API</a>
+</div>
+</body>
+</html>
